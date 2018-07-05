1d2e76c937deeb837bf6b5cd1dc3d0c4934c7be8
Ivan3424 <wwj3424v@163.com>
2018-07-05 15:49:13 +0800

    modify

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1530776907,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/bg_game-sheet0.png",
+		"images/score.png",
+		"images/claw-sheet0.png",
+		"images/claw_trigger-sheet0.png",
+		"images/gold1.png",
+		"images/gold2.png",
+		"images/diamond.png",
+		"images/bottle.png",
+		"images/shoe.png",
+		"images/obj_trigger-sheet0.png",
+		"images/bg_start-sheet0.png",
+		"images/start-sheet0.png",
+		"images/bg_over-sheet0.png",
+		"images/restart-sheet0.png",
+		"images/share-sheet0.png",
+		"images/share1-sheet0.png",
+		"images/share3-sheet0.png",
+		"images/share2-sheet0.png"
+	]
+}
